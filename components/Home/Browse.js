6bcaf986b96e831c26d2e44e0b48e108153d1eb4
thunderhead27 <thunderhead27@gmail.com>
2023-02-20 14:55:30 -0800

    fifth commit

--- components/Home/Browse.js
+++ components/Home/Browse.js
@@ -4,39 +4,45 @@ import Arrow from '../../public/assets/shared/desktop/icon-arrow-right.svg';
 
 export const Browse = () => {
   return (
-    <div tw="flex flex-col tablet:flex-row gap-x-4 laptop:gap-x-8 mt-24">
+    <div tw="flex flex-col tablet:flex-row gap-y-16 tablet:gap-x-8 pt-36">
       <Link href="/headphones">
         <div tw="flex flex-col mb-4">
-          <img tw="self-center" src="/assets/shared/desktop/image-category-thumbnail-headphones.png" srcSet="/assets/shared/desktop/image-category-thumbnail-headphones-mobile.png 400w, /assets/shared/desktop/image-category-thumbnail-headphones.png 400w" sizes="(max-width: 1439px) 200px, 400px" />
-          <div tw="flex flex-col h-40 w-80 tablet:w-48 tablet:h-40 laptop:h-52 laptop:w-[350px] relative bg-darkGray top-[-90px] tablet:top-[-90px] laptop:top-[-155px] desktop:top-[-160px] z-[-10] self-center rounded-lg pt-20 laptop:pt-32 text-center font-sans font-bold">
-            <span>HEADPHONES</span>
-            <div tw="flex flex-row items-center justify-center">
-              <span tw="opacity-50 mr-2">SHOP</span>
-              <Arrow />
+          <div tw="flex flex-col h-40 w-80 tablet:w-48 tablet:h-40 laptop:h-52 laptop:w-[350px] bg-darkGray rounded-lg pt-20 laptop:pt-32 text-center font-sans font-bold">
+            <div tw="flex flex-col relative top-[-150px] laptop:top-[-260px]">
+                <img tw="self-center relative" src="/assets/shared/desktop/image-category-thumbnail-headphones.png" srcSet="/assets/shared/desktop/image-category-thumbnail-headphones-mobile.png 400w, /assets/shared/desktop/image-category-thumbnail-headphones.png 400w" sizes="(max-width: 1439px) 200px, 400px" />
+              <span>HEADPHONES</span>
+              <div tw="flex flex-row items-center justify-center">
+                <span tw="opacity-50 mr-2">SHOP</span>
+                <Arrow />
+              </div>
             </div>
           </div>
         </div>
       </Link>
       <Link href="/speakers">
         <div tw="flex flex-col">
-          <img tw="self-center" src="/assets/shared/desktop/image-category-thumbnail-speakers.png" srcSet="/assets/shared/desktop/image-category-thumbnail-speakers-mobile.png 400w, /assets/shared/desktop/image-category-thumbnail-speakers.png, 400w" sizes="(max-width: 1439px) 200px, 400px" />
-          <div tw="flex flex-col h-40 w-80 tablet:w-48 tablet:h-40 laptop:h-52 laptop:w-[350px] relative bg-darkGray top-[-90px] tablet:top-[-86px] laptop:top-[-145px] desktop:top-[-151px] z-[-10] self-center rounded-lg pt-20 laptop:pt-32 text-center font-sans font-bold">
-            <span>SPEAKERS</span>
-            <div tw="flex flex-row items-center justify-center">
-              <span tw="opacity-50 mr-2">SHOP</span>
-              <Arrow />
+          <div tw="flex flex-col h-40 w-80 tablet:w-48 tablet:h-40 laptop:h-52 laptop:w-[350px] bg-darkGray z-[-10] self-center rounded-lg pt-20 laptop:pt-32 text-center font-sans font-bold">
+            <div tw="flex flex-col relative top-[-140px] laptop:top-[-250px]">
+            <img tw="self-center relative" src="/assets/shared/desktop/image-category-thumbnail-speakers.png" srcSet="/assets/shared/desktop/image-category-thumbnail-speakers-mobile.png 400w, /assets/shared/desktop/image-category-thumbnail-speakers.png, 400w" sizes="(max-width: 1439px) 200px, 400px" />
+              <span>SPEAKERS</span>
+              <div tw="flex flex-row items-center justify-center">
+                <span tw="opacity-50 mr-2">SHOP</span>
+                <Arrow />
+              </div>
             </div>
           </div>
         </div>
       </Link>
       <Link href="earphones">
         <div tw="flex flex-col">
-          <img tw="self-center" src="/assets/shared/desktop/image-category-thumbnail-earphones.png" srcSet="assets/shared/desktop/image-category-thumbnail-earphones-mobile.png 400w, /assets/shared/desktop/image-category-thumbnail-earphones.png, 400w" sizes="(max-width: 1439px) 200px, 400px" />
-          <div tw="flex flex-col h-40 w-80 tablet:w-48 tablet:h-40 laptop:h-52 laptop:w-[350px] relative bg-darkGray top-[-90px] tablet:top-[-78px] laptop:top-[-128px] desktop:top-[-133px] z-[-10] self-center rounded-lg pt-20 laptop:pt-32 text-center font-sans font-bold">
-            <span>EARPHONES</span>
-            <div tw="flex flex-row items-center justify-center">
-              <span tw="opacity-50 mr-2">SHOP</span>
-              <Arrow />
+          <div tw="flex flex-col h-40 w-80 tablet:w-48 tablet:h-40 laptop:h-52 laptop:w-[350px] bg-darkGray z-[-10] self-center rounded-lg pt-20 laptop:pt-32 text-center font-sans font-bold">
+            <div tw="flex flex-col relative top-[-130px] laptop:top-[-230px]">
+                <img tw="self-center relative" src="/assets/shared/desktop/image-category-thumbnail-earphones.png" srcSet="assets/shared/desktop/image-category-thumbnail-earphones-mobile.png 400w, /assets/shared/desktop/image-category-thumbnail-earphones.png, 400w" sizes="(max-width: 1439px) 200px, 400px" />
+              <span>EARPHONES</span>
+              <div tw="flex flex-row items-center justify-center">
+                <span tw="opacity-50 mr-2">SHOP</span>
+                <Arrow />
+              </div>
             </div>
           </div>
         </div>
